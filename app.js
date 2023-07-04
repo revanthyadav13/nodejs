513943c1c13833ee7bcb712dd3b769e0976654d9
revanthyadav13 <revanthyadav13@gmail.com>
2023-07-04 13:10:01 +0530

    Custom reponses based on url

--- app.js
+++ app.js
@@ -2,12 +2,35 @@ const http=require('http');
 
 const server=http.createServer((req,res)=>{
 console.log(req.url, req.method, req.headers);
-res.setHeader('Content-Type','text/html');
-res.write('<html>');
-res.write('<head><title>This is my first page</title></head>');
-res.write('<body><h1>Revanth Yadav</h1></body>');
+
+
+if(req.url=='/home'){
+    res.setHeader('Content-Type','text/html');
+   res.write('<html>');
+res.write('<head><title>Home page</title></head>');
+res.write('<body><h1>Welcome home</h1></body>');
 res.write('</html>');
 res.end();
+}
+
+else if(req.url== '/about'){
+    res.setHeader('Content-Type','text/html');
+  res.write('<html>');
+res.write('<head><title>About page</title></head>');
+res.write('<body><h1> Welcome to About Us page</h1></body>');
+res.write('</html>');
+res.end();  
+}
+
+else{
+    res.setHeader('Content-Type','text/html');
+res.write('<html>');
+res.write('<head><title>Js project page</title></head>');
+res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+res.write('</html>');
+res.end();
+}
+
 });
 
 
