26860d957f4f08708be48d42828227932187a301
revanthyadav13 <revanthyadav13@gmail.com>
2023-07-22 11:46:38 +0530

    User and product relationship

--- app.js
+++ app.js
@@ -4,41 +4,55 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
-const sequelize=require('./util/database');
-
-const User=require('./models/user');
-
-const cors=require('cors');
 const app = express();
 
-
-app.use(cors());
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const userRoutes=require('./routes/user');
-const stockRoutes=require('./routes/stock');
 
-app.use(bodyParser.json({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-app.use('/user',userRoutes);
-app.use('/stock',stockRoutes);
 
 app.use(errorController.get404);
 
-
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
 
 sequelize
-.sync()
-.then(result=>{
+  // .sync({ force: true })
+  .sync()
+  .then(result => {
+    return User.findByPk(1);
+    // console.log(result);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({ name: 'Max', email: 'test@test.com' });
+    }
+    return user;
+  })
+  .then(user => {
+    // console.log(user);
     app.listen(3000);
-})
-.catch(err=>{console.log(err)});
-
-
+  })
+  .catch(err => {
+    console.log(err);
+  });
--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
-const Sequelize= require('sequelize');
+const Sequelize = require('sequelize');
 
-const sequelize=require('../util/database');
+const sequelize = require('../util/database');
 
 const User = sequelize.define('user', {
   id: {
@@ -9,17 +9,8 @@ const User = sequelize.define('user', {
     allowNull: false,
     primaryKey: true
   },
-  username: Sequelize.STRING,
-  phonenumber: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    unique:true
-  },
-  email: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    unique:true
-  }
+  name: Sequelize.STRING,
+  email: Sequelize.STRING
 });
 
 module.exports = User;
